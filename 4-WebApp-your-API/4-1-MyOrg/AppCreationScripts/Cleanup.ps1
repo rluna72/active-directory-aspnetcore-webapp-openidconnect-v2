e2b9523c9386f8fc929d24708f45a29427af729b
Alexander Beyderman <v-abeyderman@microsoft.com>
2021-12-16 22:07:26 -0500

    update for sample 4-1

--- 4-WebApp-your-API/4-1-MyOrg/AppCreationScripts/Cleanup.ps1
+++ 4-WebApp-your-API/4-1-MyOrg/AppCreationScripts/Cleanup.ps1
@@ -7,13 +7,12 @@ param(
     [string] $azureEnvironmentName
 )
 
-#Requires -Modules AzureAD -RunAsAdministrator
+#Requires -Modules Microsoft.Graph.Applications
 
-
-if ($null -eq (Get-Module -ListAvailable -Name "AzureAD")) { 
-    Install-Module "AzureAD" -Scope CurrentUser 
+if ($null -eq (Get-Module -ListAvailable -Name "Microsoft.Graph.Applications")) { 
+    Install-Module "Microsoft.Graph.Applications" -Scope CurrentUser                                            
 } 
-Import-Module AzureAD
+Import-Module Microsoft.Graph.Applications
 $ErrorActionPreference = "Stop"
 
 Function Cleanup
@@ -31,66 +30,80 @@ Function Cleanup
     # $tenantId is the Active Directory Tenant. This is a GUID which represents the "Directory ID" of the AzureAD tenant 
     # into which you want to create the apps. Look it up in the Azure portal in the "Properties" of the Azure AD. 
 
-    # Login to Azure PowerShell (interactive if credentials are not already provided:
-    # you'll need to sign-in with creds enabling your to create apps in the tenant)
-    if (!$Credential -and $TenantId)
-    {
-        $creds = Connect-AzureAD -TenantId $tenantId -AzureEnvironmentName $azureEnvironmentName
-    }
-    else
-    {
-        if (!$TenantId)
-        {
-            $creds = Connect-AzureAD -Credential $Credential -AzureEnvironmentName $azureEnvironmentName
-        }
-        else
-        {
-            $creds = Connect-AzureAD -TenantId $tenantId -Credential $Credential -AzureEnvironmentName $azureEnvironmentName
-        }
-    }
-
-    if (!$tenantId)
-    {
-        $tenantId = $creds.Tenant.Id
-    }
-    $tenant = Get-AzureADTenantDetail
-    $tenantName =  ($tenant.VerifiedDomains | Where-Object { $_._Default -eq $True }).Name
+    # Connect to the Microsoft Graph API
+    Write-Host "Connecting Microsoft Graph"
+    Connect-MgGraph -TenantId $TenantId -Scopes "Application.ReadWrite.All"
     
     # Removes the applications
-    Write-Host "Cleaning-up applications from tenant '$tenantName'"
+    Write-Host "Cleaning-up applications from tenant '$tenantId'"
 
-    Write-Host "Removing 'service' (TodoListService-aspnetcore-webapi) if needed"
-    Get-AzureADApplication -Filter "DisplayName eq 'TodoListService-aspnetcore-webapi'"  | ForEach-Object {Remove-AzureADApplication -ObjectId $_.ObjectId }
-    $apps = Get-AzureADApplication -Filter "DisplayName eq 'TodoListService-aspnetcore-webapi'"
+    Write-Host "Removing 'service' (New_TodoListService-aspnetcore-webapi) if needed"
+    try
+    {
+        Get-MgApplication -Filter "DisplayName eq 'New_TodoListService-aspnetcore-webapi'"  | ForEach-Object {Remove-MgApplication -ApplicationId $_.Id }
+    }
+    catch
+    {
+	    Write-Host "Unable to remove the 'New_TodoListService-aspnetcore-webapi' . Try deleting manually." -ForegroundColor White -BackgroundColor Red
+    }
+
+    Write-Host "Making sure there are no more (New_TodoListService-aspnetcore-webapi) applications found, will remove if needed..."
+    $apps = Get-MgApplication -Filter "DisplayName eq 'New_TodoListService-aspnetcore-webapi'"
+    
     if ($apps)
     {
-        Remove-AzureADApplication -ObjectId $apps.ObjectId
+        Remove-MgApplication -ApplicationId $apps.Id
     }
 
     foreach ($app in $apps) 
     {
-        Remove-AzureADApplication -ObjectId $app.ObjectId
-        Write-Host "Removed TodoListService-aspnetcore-webapi.."
+        Remove-MgApplication -ApplicationId $app.Id
+        Write-Host "Removed New_TodoListService-aspnetcore-webapi.."
     }
+
     # also remove service principals of this app
-    Get-AzureADServicePrincipal -filter "DisplayName eq 'TodoListService-aspnetcore-webapi'" | ForEach-Object {Remove-AzureADServicePrincipal -ObjectId $_.Id -Confirm:$false}
+    try
+    {
+        Get-MgServicePrincipal -filter "DisplayName eq 'New_TodoListService-aspnetcore-webapi'" | ForEach-Object {Remove-MgServicePrincipal -ApplicationId $_.Id -Confirm:$false}
+    }
+    catch
+    {
+	    Write-Host "Unable to remove ServicePrincipal 'New_TodoListService-aspnetcore-webapi' . Try deleting manually from Enterprise applications." -ForegroundColor White -BackgroundColor Red
+    }
+    Write-Host "Removing 'client' (New_TodoListClient-aspnetcore-webapi) if needed"
+    try
+    {
+        Get-MgApplication -Filter "DisplayName eq 'New_TodoListClient-aspnetcore-webapi'"  | ForEach-Object {Remove-MgApplication -ApplicationId $_.Id }
+    }
+    catch
+    {
+	    Write-Host "Unable to remove the 'New_TodoListClient-aspnetcore-webapi' . Try deleting manually." -ForegroundColor White -BackgroundColor Red
+    }
+
+    Write-Host "Making sure there are no more (New_TodoListClient-aspnetcore-webapi) applications found, will remove if needed..."
+    $apps = Get-MgApplication -Filter "DisplayName eq 'New_TodoListClient-aspnetcore-webapi'"
     
-    Write-Host "Removing 'client' (TodoListClient-aspnetcore-webapi) if needed"
-    Get-AzureADApplication -Filter "DisplayName eq 'TodoListClient-aspnetcore-webapi'"  | ForEach-Object {Remove-AzureADApplication -ObjectId $_.ObjectId }
-    $apps = Get-AzureADApplication -Filter "DisplayName eq 'TodoListClient-aspnetcore-webapi'"
     if ($apps)
     {
-        Remove-AzureADApplication -ObjectId $apps.ObjectId
+        Remove-MgApplication -ApplicationId $apps.Id
     }
 
     foreach ($app in $apps) 
     {
-        Remove-AzureADApplication -ObjectId $app.ObjectId
-        Write-Host "Removed TodoListClient-aspnetcore-webapi.."
+        Remove-MgApplication -ApplicationId $app.Id
+        Write-Host "Removed New_TodoListClient-aspnetcore-webapi.."
     }
+
     # also remove service principals of this app
-    Get-AzureADServicePrincipal -filter "DisplayName eq 'TodoListClient-aspnetcore-webapi'" | ForEach-Object {Remove-AzureADServicePrincipal -ObjectId $_.Id -Confirm:$false}
-    
+    try
+    {
+        Get-MgServicePrincipal -filter "DisplayName eq 'New_TodoListClient-aspnetcore-webapi'" | ForEach-Object {Remove-MgServicePrincipal -ApplicationId $_.Id -Confirm:$false}
+    }
+    catch
+    {
+	    Write-Host "Unable to remove ServicePrincipal 'New_TodoListClient-aspnetcore-webapi' . Try deleting manually from Enterprise applications." -ForegroundColor White -BackgroundColor Red
+    }
 }
 
-Cleanup -Credential $Credential -tenantId $TenantId
+Cleanup -tenantId $TenantId
+
